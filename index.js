c8671597523fc5b12b4e46b335c8e3cdf1334421
eepp99 <ezennaeronini@gmail.com>
2025-06-23 13:52:08 +0100

    index.js

--- index.js
+++ index.js
@@ -7,7 +7,8 @@ const nodemailer = require('nodemailer');
 require('dotenv').config();
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
+
 
 // Middleware
 app.use(cors());
